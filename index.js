5c6de73957613ece2ec37bb44de52cc1d7451b4f
mfoy92 <michaelfoy92@googlemail.com>
2022-11-17 17:15:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -15,19 +15,19 @@ convertBtn.addEventListener("click", function() {
     const inputValue = inputEl.value
     const metre = inputValue   
     const feet = metre * 3.281
-    lengthOutput.textContent = `${metre.toFixed(3)} metres = ${feet.toFixed(3)} feet / ${feet.toFixed(3) } feet = ${metre.toFixed(3)} metres`   
+    lengthOutput.textContent = `${Number(metre).toFixed(3)} metres = ${feet.toFixed(3)} feet / ${feet.toFixed(3) } feet = ${Number(metre).toFixed(3)} metres`   
 })
 
-//convertBtn.addEventListener("click", function() {
-    //const inputValue = inputEl.value
-    //const litre = inputValue   
-    //const gallon = litre * 0.264
-    //volumeOutput.textContent = `${litre.toFixed(3)} litres = ${gallon.toFixed(3)} gallons / ${gallon.toFixed(3) } gallons = ${litre.toFixed(3)} litres`   
-//})
+convertBtn.addEventListener("click", function() {
+    const inputValue = inputEl.value
+    const litre = inputValue   
+    const gallon = litre * 0.264
+    volumeOutput.textContent = `${Number(litre).toFixed(3)} litres = ${gallon.toFixed(3)} gallons / ${gallon.toFixed(3) } gallons = ${Number(litre).toFixed(3)} litres`   
+})
 
-//convertBtn.addEventListener("click", function() {
-    //const inputValue = inputEl.value
-    //const kilogram = inputValue   
-    //const pound = kilogram * 2.204
-    //massOutput.textContent = `${kilogram.toFixed(3)} kilograms = ${pound.toFixed(3)} pounds / ${pound.toFixed(3) } pounds = ${kilogram.toFixed(3)} kilograms`   
-//})
+convertBtn.addEventListener("click", function() {
+    const inputValue = inputEl.value
+    const kilogram = inputValue   
+    const pound = kilogram * 2.204
+    massOutput.textContent = `${Number(kilogram).toFixed(3)} kilograms = ${pound.toFixed(3)} pounds / ${pound.toFixed(3) } pounds = ${Number(kilogram).toFixed(3)} kilograms`   
+})
